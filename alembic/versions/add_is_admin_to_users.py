a4d071b35f363149638623e166dd847d4ea68858
nbcr <840382+nbcr@users.noreply.github.com>
2025-11-21 11:28:20 -0500

    Add Alembic migration to add is_admin column to users table

---
+++ alembic/versions/add_is_admin_to_users.py
@@ -0,0 +1,17 @@
+"""
+Add is_admin column to users table
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'add_is_admin_to_users'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.add_column('users', sa.Column('is_admin', sa.Boolean(), nullable=False, server_default=sa.text('false')))
+
+def downgrade():
+    op.drop_column('users', 'is_admin')
