7e25eab2d1319aee5abb442eff4df5a9f51abe41
nbcr <840382+nbcr@users.noreply.github.com>
2025-11-24 18:20:41 -0500

    Remove all code after upgrade header to fix Alembic migration indentation error

--- alembic/versions/add_is_admin_to_users.py
+++ alembic/versions/add_is_admin_to_users.py
@@ -10,7 +10,6 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
-def upgrade():
-    # op.add_column('users', sa.Column('is_admin', sa.Boolean(), nullable=False, server_default=sa.text('false')))
+
 
 
