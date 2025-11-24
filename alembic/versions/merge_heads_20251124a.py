c7f5fa5d9f21243f2708b3d2ef900106084a6453
nbcr <840382+nbcr@users.noreply.github.com>
2025-11-24 18:31:17 -0500

    Update merge migration to reference all Alembic heads

--- alembic/versions/merge_heads_20251124a.py
+++ alembic/versions/merge_heads_20251124a.py
@@ -5,7 +5,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = 'merge_heads_20251124a'
-down_revision = ('003', 'add_bio_to_uip', 'add_is_admin_to_users')
+down_revision = ('003', 'add_bio_to_uip', 'add_is_admin_to_users', 'add_bio_to_user_interest_profiles')
 branch_labels = None
 depends_on = None
 
